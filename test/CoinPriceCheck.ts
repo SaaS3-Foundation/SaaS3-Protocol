a1e2b20947421cd57a1e25acaf94e529f48d0b9c
songtianyi <tianyi@saas3.io>
2023-02-16 15:49:13 +0800

    add CoinPrice check script

--- test/CoinPriceCheck.ts
+++ test/CoinPriceCheck.ts
@@ -7,10 +7,11 @@ async function main() {
 
     console.log("Account balance:", (await deployer.getBalance()).toString());
 
-    const CoinPrice = await ethers.getContractFactory("CoinPrice");
+    const CoinPrice= await ethers.getContractFactory("CoinPrice");
     const c = await CoinPrice.attach('0x7cAf6684538d6DfBfa72fA0f2cFb504059724A32');
     console.log("CoinPrice Contract address:", c.address);
-    await c.connect(deployer).set_oracle('0x03B456708e05B447d88Ae4EaC0A0afF66f8d166a', '0x962D0Ca297F21f027E9d25613F2D53aAa31F5fCC');
+    let v = await c.connect(deployer).result();
+    console.log(v);
 }
 
 main();
